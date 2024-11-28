a7dece2af11a6c07f0c15f63f8eeb6af006411a3
Muhammadmaaz <kolemaaz6@gmail.com>
2024-11-28 18:51:53 +0530

    Termwork 1

---
+++ termwork1-Simplecalculator.c
@@ -0,0 +1,19 @@
+//Termwork 1 simple calculator using switch
+#include<stdio.h>
+int main(){
+    float a,b;
+    char op;
+    printf("Enter the expression:-->");
+    scanf("%f%c%f",&a,&op,&b);
+    switch(op){
+        case '+':printf("Result = %f",a+b);
+        break;
+        case '-':printf("Result = %f",a-b);
+        break;
+        case '*':printf("Result = %f",a*b);
+        break;
+        case '/':printf("Result = %f",a/b);
+        break;
+    }
+    return 0;
+}
