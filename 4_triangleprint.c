688b6e7835c8013f187d3dac7302376604489a7b
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-17 15:47:24 +0530

    College Termworks!!!

--- 4_triangleprint.c
+++ 4_triangleprint.c
@@ -1,14 +1,45 @@
-#include<stdio.h>
-int main(){
-    int rows,i,x,j;
-    x=1;
-    printf("Enter the number of rows : ");
-    scanf("%d",&rows);
-    for(i=1;i<=rows;i++){
-        for(j=0;j<i;j++){
-            printf("%d\t",x++);
-        }
-        printf("\n");
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Input the size of the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the elements of the array
+    printf("Enter %d elements which need to sorted  :\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
+
+    // Print the unsorted array
+    printf("Unsorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Bubble Sort algorithm
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print the sorted array
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
