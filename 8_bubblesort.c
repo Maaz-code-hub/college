27efd2e2dd692a07703185a0cf10689d9d26742e
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-17 20:06:17 +0530

    College Termworks!!!

--- 8_bubblesort.c
+++ 8_bubblesort.c
@@ -1,28 +1,45 @@
-#include<stdio.h>      //1 4 2 5 3 9 5 try it once
-int main(){
-    int Arr[100];
-    int n,i,j;
-    int temp;
-    printf("Enter the number of elements : ");
-    scanf("%d",&n);
-    printf("Enter th elements need to be sorted :\n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&Arr[i]);
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Input the size of the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the elements of the array
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    //Bubble sorting process
-    
-        for(i=0;i<n;i++){
-        for(j=0;j<n-i-1;j++){
-            if(Arr[i]>Arr[i+1]){
-                temp=Arr[i];
-                Arr[i]=Arr[i+1];
-                Arr[i+1]=temp;
+
+    // Print the unsorted array
+    printf("Unsorted array:-->\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Bubble Sort algorithm
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        }
-    printf("The sorted array is -->\n");
-    for(i=0;i<n;i++){
-        printf("%d\t",Arr[i]);
     }
+
+    // Print the sorted array
+    printf("Sorted array:-->\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
