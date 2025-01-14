22fac71895bae1619ecc95fee82979486855cc56
Muhammadmaaz <kolemaaz6@gmail.com>
2025-01-14 14:24:20 +0530

    College Termworks!!

--- 4_triangleprint.c
+++ 4_triangleprint.c
@@ -1,45 +1,12 @@
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
     int n;
-
-    // Input the size of the array
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    // Input the elements of the array
-    printf("Enter %d elements which need to sorted  :\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Print the unsorted array
-    printf("Unsorted array:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    // Bubble Sort algorithm
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+    printf("Enter the number of lines : ");
+    scanf("%d",&n);
+    for(int i = 1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            printf("*");
         }
+        printf("\n");
     }
-
-    // Print the sorted array
-    printf("Sorted array:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
 }
