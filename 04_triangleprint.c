afc4fd2732f19383058c31427fd8063131e0d370
Muhammadmaaz <kolemaaz6@gmail.com>
2025-01-14 15:16:10 +0530

    College Termworks!!

--- 04_triangleprint.c
+++ 04_triangleprint.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-    int n;
+    int n,x;
+    x=1;
     printf("Enter the number of lines : ");
     scanf("%d",&n);
     for(int i = 1;i<=n;i++){
         for(int j=0;j<i;j++){
-            printf("*");
+            printf("%d\t",x++);
         }
         printf("\n");
     }
